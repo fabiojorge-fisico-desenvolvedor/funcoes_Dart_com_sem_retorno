eb4ea561f5ec66e4d7446af1172113a368755e2b
fabiojorge-fisico-desenvolvedor <fabiomendes@edu.se.df.gov.br>
2021-08-07 12:48:28 -0300

    funcao sem retorno e com parâmetros

--- bin/dart_2_com_ou_sem_retorno.dart
+++ bin/dart_2_com_ou_sem_retorno.dart
@@ -4,9 +4,14 @@ import 'package:dart_2_com_ou_sem_retorno/dart_2_com_ou_sem_retorno.dart'
 import 'dart:io';
 
 void main(List<String> arguments) {
-  imprimir();
+  imprimirNome('joão');
 }
-// funcao sem retorno e sem parâmetros
+
+// funcao sem retorno e com parâmetros
 void imprimir() {
   print('olá pessoal');
+}
+
+void imprimirNome(String nome) {
+  print('olá $nome');
 }
