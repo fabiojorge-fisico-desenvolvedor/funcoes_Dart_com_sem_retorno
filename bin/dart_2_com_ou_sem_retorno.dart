aabbfadb7c38e82a3b698591aad43e9d2267ec82
fabiojorge-fisico-desenvolvedor <fabiomendes@edu.se.df.gov.br>
2021-08-07 12:33:43 -0300

    teste de funcionamento

---
+++ bin/dart_2_com_ou_sem_retorno.dart
@@ -0,0 +1,5 @@
+import 'package:dart_2_com_ou_sem_retorno/dart_2_com_ou_sem_retorno.dart' as dart_2_com_ou_sem_retorno;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_2_com_ou_sem_retorno.calculate()}!');
+}
---
+++ test/dart_2_com_ou_sem_retorno_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_2_com_ou_sem_retorno/dart_2_com_ou_sem_retorno.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
