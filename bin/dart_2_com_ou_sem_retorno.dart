5bc33bf7122c789293a481f97a46e2ef80d1b95a
fabiojorge-fisico-desenvolvedor <fabiomendes@edu.se.df.gov.br>
2021-08-07 12:42:42 -0300

    sem retorno e sem parâmetro

--- bin/dart_2_com_ou_sem_retorno.dart
+++ bin/dart_2_com_ou_sem_retorno.dart
@@ -1,5 +1,12 @@
-import 'package:dart_2_com_ou_sem_retorno/dart_2_com_ou_sem_retorno.dart' as dart_2_com_ou_sem_retorno;
+import 'package:dart_2_com_ou_sem_retorno/dart_2_com_ou_sem_retorno.dart'
+    as dart_2_com_ou_sem_retorno;
+
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_2_com_ou_sem_retorno.calculate()}!');
+  imprimir();
+}
+// funcao sem retorno e sem parâmetros
+void imprimir() {
+  print('olá pessoal');
 }
