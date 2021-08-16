d2d141b7562cb33bf02127b649544ee1fc30ea95
fabiojorge-fisico-desenvolvedor <fabiomendes@edu.se.df.gov.br>
2021-08-16 16:14:21 -0300

    com retorno e sem parametro

--- bin/dart_2_com_ou_sem_retorno.dart
+++ bin/dart_2_com_ou_sem_retorno.dart
@@ -4,7 +4,10 @@ import 'package:dart_2_com_ou_sem_retorno/dart_2_com_ou_sem_retorno.dart'
 import 'dart:io';
 
 void main(List<String> arguments) {
-  imprimirNome('Jorge');
+  //imprimirNome('Jorge');// necessário alterar para função a ser utilizada
+
+  var mensagem = devolverOla();
+  print(mensagem);
 }
 /*sem retorno e sem parâmetros (basta liberar a função comentario dessa região)
  void imprimir (){
@@ -14,6 +17,13 @@ void main(List<String> arguments) {
 
 //função sem retorno e com parâmetro
 
-void imprimirNome(String nome) {
+/*void imprimirNome(String nome) {
   print('olá $nome');
+}*/
+
+//com retorno e sem parâmetro
+
+String devolverOla() {
+  // com retorno chamo a string e coloco a palavra return para retornar, por exemplo, sem parâmetro ...nada dentro do parentese
+  return 'olá pessoal!';
 }
