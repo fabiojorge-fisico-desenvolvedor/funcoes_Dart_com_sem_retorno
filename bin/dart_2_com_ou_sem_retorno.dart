3e7c9dd1c2450e4a105b74331a9bca9389745279
fabiojorge-fisico-desenvolvedor <fabiomendes@edu.se.df.gov.br>
2021-08-17 16:07:36 -0300

    tudo dentro do print com if else reduzido

--- bin/dart_2_com_ou_sem_retorno.dart
+++ bin/dart_2_com_ou_sem_retorno.dart
@@ -4,17 +4,14 @@ import 'package:dart_2_com_ou_sem_retorno/dart_2_com_ou_sem_retorno.dart'
 import 'dart:io';
 
 void main(List<String> arguments) {
-  imprimirOla(
-      'Joaquim'); // eu retiri o valor 1 daqui, assim o padrão vai automaticamente para o zero.
+  imprimirOla('Joaquim',
+      opcao:
+          1); // eu retiri o valor 1 daqui, assim o padrão vai automaticamente para o zero.
 }
 
 void imprimirOla(String nome, {int opcao = 0}) {
   //=====> NOmeado coloca a chave porque aí a posição não importa deixando ele opcional
-  print('ola $nome!');
-
-  var mensagem =
-      (opcao == 0) ? 'Tudo bem?' : 'como vai?'; //==> um if else mais curto
-  print(mensagem);
+  print('ola $nome! ${(opcao == 0) ? 'Tudo bem?' : 'como vai?'}');
 }
 
 /*sem retorno e sem parâmetros (basta liberar a função comentario dessa região)
